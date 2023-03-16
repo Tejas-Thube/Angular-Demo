22dca8cdd7f85d7ef528462cb13144473984b651
tejast <tejasthube3030@gmail.com>
2023-03-16 13:43:09 +0530

    signup

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  constructor() { }
 
+  ngOnInit(): void {
+    
+  }
 }
